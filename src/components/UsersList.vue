f4c09c9208b5c83924d4764a08a241db015758d2
Fedor Sharaputo <sharaputo@icloud.com>
2023-01-19 06:14:09 +0300

    transitions added

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -7,31 +7,33 @@
     v-for="(user, i) in users"
     :key="`${user.title}-${i}`"
   >
-    <v-card max-width="450" class="mx-auto">
-      <v-card-item>
-        <v-menu min-width="200px" rounded>
-          <template v-slot:activator="{ props }">
-            <v-btn icon size="x-large" class="mb-3" v-bind="props">
-              <v-avatar size="64">
-                <v-img :src="user.avatar"></v-img>
-              </v-avatar>
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-text>
-              <div class="mx-auto text-center">
-                <p class="text-caption mt-1">
-                  {{ user.address }}
-                </p>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-menu>
+    <Transition name="scale" appear>
+      <v-card max-width="450" class="mx-auto">
+        <v-card-item>
+          <v-menu min-width="200px" rounded>
+            <template v-slot:activator="{ props }">
+              <v-btn icon size="x-large" class="mb-3" v-bind="props">
+                <v-avatar size="64">
+                  <v-img :src="user.avatar"></v-img>
+                </v-avatar>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-text>
+                <div class="mx-auto text-center">
+                  <p class="text-caption mt-1">
+                    {{ user.address }}
+                  </p>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-menu>
 
-        <v-card-title class="mb-2" v-html="user.title"></v-card-title>
-        <v-card-subtitle v-html="user.subtitle"></v-card-subtitle>
-      </v-card-item>
-    </v-card>
+          <v-card-title class="mb-2" v-html="user.title"></v-card-title>
+          <v-card-subtitle v-html="user.subtitle"></v-card-subtitle>
+        </v-card-item>
+      </v-card>
+    </Transition>
   </v-col>
 </template>
 
@@ -45,4 +47,19 @@ interface Props {
 defineProps<Props>();
 </script>
 
-<style scoped></style>
+<style scoped>
+.scale-enter-active {
+  animation: scale 0.3s ease-in-out;
+}
+.scale-leave-active {
+  animation: scale 0.3s ease-in-out reverse;
+}
+@keyframes scale {
+  0% {
+    transform: scale(0.7);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
